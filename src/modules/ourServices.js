5f25009d1536893471f5ce1a3cf2628c36256b91
Dinieva Rezeda <dinievarezeda@mail.ru>
2023-01-19 14:29:41 +0300

    анимация, адаптация слайдера "наши услуги"

--- src/modules/ourServices.js
+++ src/modules/ourServices.js
@@ -1,10 +1,11 @@
 const ourServices = () => {
-    const sliderContainer = document.querySelector('.row');
     const slides = document.querySelectorAll('.col-md-12.col-lg-6');
     const buttonsBlock = document.querySelector('.services-arrows');
     const windowOuterWidth = window.outerWidth;
     let showSlideAmount; //количество слайдов в зависимости от размера экрана
-    console.log(sliderContainer.lastChild);
+    const timeInterval = 2000;
+    let interval;
+
     const showSlides = () => {
         slides.forEach((slide, index) => {
             // slide.style.transition = '2s';
@@ -12,7 +13,6 @@ const ourServices = () => {
                 slide.style.display = "none"
             } else {
                 slide.style.display = "block"
-                slide.id = 'activeSlide'
             }
         })
     }
@@ -21,10 +21,8 @@ const ourServices = () => {
         slides.forEach((slide) => {
             if (slide.style.display == 'block') {
                 slide.style.display = 'none'
-                slide.removeAttribute('id')
             } else if (slide.style.display == 'none') {
                 slide.style.display = 'block'
-                slide.id = 'activeSlide'
             }
         });
     }
@@ -32,6 +30,15 @@ const ourServices = () => {
     if (windowOuterWidth > 576) {
         showSlideAmount = 2;
         showSlides();
+
+        const startSlide = (timer = 1500) => {
+            interval = setInterval(changeSlides, timer)
+        }
+
+        const stoptSlide = () => {
+            clearInterval(interval)
+        }
+
         buttonsBlock.addEventListener('click', (e) => {
             if (e.target.closest('.services__arrow--right')) {
                 changeSlides();
@@ -41,45 +48,73 @@ const ourServices = () => {
                 changeSlides();
             }
         });
+
+        //останавливаем таймер при наведении курсора на стрелки
+        buttonsBlock.addEventListener('mouseenter', (e) => {
+            if (e.target.matches('.services__arrow ')) {
+                stoptSlide()
+            }
+        }, true)
+
+        //запускаем таймер при наведении курсора на стрелки
+        buttonsBlock.addEventListener('mouseleave', (e) => {
+            if (e.target.matches('.services__arrow ')) {
+                startSlide(timeInterval)
+            }
+        }, true)
+
+        startSlide(timeInterval) //запускаем таймер
+
     } else if (windowOuterWidth < 576) {
         showSlideAmount = 1
         showSlides()
+        let currentSlide = 0;
+
+        const prevSlide = (elems, index) => {
+            elems[index].style.display = 'none'
+        }
+
+        const nextSlide = (elems, index) => {
+            elems[index].style.display = 'block'
+        }
+
+        const autoSlide = () => {
+            prevSlide(slides, currentSlide)
+            currentSlide++
+
+            if (currentSlide >= slides.length) {
+                currentSlide = 0
+            }
+
+            nextSlide(slides, currentSlide)
+        }
+
+        const startSlide = (timer = 1500) => {
+            interval = setInterval(autoSlide, timer)
+        }
 
         buttonsBlock.addEventListener('click', (e) => {
-            let activeSlide = document.getElementById('activeSlide');
+            e.preventDefault();
+            prevSlide(slides, currentSlide)
             if (e.target.closest('.services__arrow--right')) {
-                slides.forEach((slide) => {
-                    if (slide.style.display == 'block') {
-                        slide.style.display = "none"
-                        activeSlide.removeAttribute('id')
-                    }
-                });
-                let nextSlide = activeSlide.nextElementSibling;
+                currentSlide++
 
-
-                if (nextSlide.closest('.col-md-12.col-lg-6')) {
-                    nextSlide.style.display = "block"
-                    nextSlide.id = 'activeSlide'
-                }
-
-            };
-
-            if (e.target.closest('.services__arrow--left')) {
-                slides.forEach((slide) => {
-                    if (slide.style.display == 'block') {
-                        slide.style.display = 'none'
-                        activeSlide.removeAttribute('id')
-                    }
-                });
-                let prevSlide = activeSlide.previousElementSibling;
-                prevSlide.style.display = "block"
-                prevSlide.id = 'activeSlide'
-
-                /* if (prevSlide == sliderContainer.firstChild) {
-                    console.log(sliderContainer.firstChild);
-                } */
+            } else if (e.target.closest('.services__arrow--left')) {
+                currentSlide--
             }
+
+            if (currentSlide >= slides.length) {
+                currentSlide = 0
+            }
+
+            if (currentSlide < 0) {
+                currentSlide = slides.length - 1
+            }
+
+            nextSlide(slides, currentSlide)
         })
+
+        startSlide(timeInterval)
     }
 }
 
