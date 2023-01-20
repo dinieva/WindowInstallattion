2b9dcd359373470bb5b778d4c7a0e7b740bf5acb
Dinieva Rezeda <dinievarezeda@mail.ru>
2023-01-21 01:51:20 +0300

    модальное окно с увеличением изображения

---
+++ src/modules/documents.js
@@ -0,0 +1,40 @@
+const documents = () => {
+    const sertificates = document.querySelectorAll('.sertificate-document')
+    const overlays = document.querySelectorAll('.document-overlay')
+    const overlayforModal = document.querySelector('.overlay')
+
+
+    sertificates.forEach(image => {
+        image.addEventListener('mouseover', (e) => {
+            e.preventDefault()
+            e.target.style.opacity = '0.9';
+        })
+        image.addEventListener('mouseout', (e) => {
+            e.preventDefault()
+            e.target.style.opacity = '0';
+        })
+    })
+
+    const modalImage = (image) => {
+        /*  overlayforModal.style.display = 'block' */
+        let modal = document.querySelector('.modal');
+        let modalImg = document.querySelector('.modal-content');
+        modal.style.display = "block";
+        modalImg.src = image.src;
+
+        let span = document.getElementsByClassName("close")[0];
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+    }
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', function (e) {
+            e.preventDefault()
+            e.target.style.opacity = '0';
+            modalImage(e.target.parentElement.querySelector('img'))
+
+        })
+    })
+}
+
+export default documents
