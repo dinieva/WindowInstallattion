6bad6a27ab5c4fb1bc9abf6bba4607ea0a9e7ab3
Dinieva Rezeda <dinievarezeda@mail.ru>
2023-01-20 14:17:35 +0300

    добавлено событие resize для window

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import benefitsSlider from './modules/benefitsSlider'
 import ourServices from './modules/ourServices'
 import requireMaster from './modules/requireMaster'
 import timer from './modules/timer'
+import sendForm from './modules/sendForm'
 
 requestCall()
 benefitsSlider()
 ourServices()
 requireMaster()
 timer('22 january 2023 21:00:00')
+sendForm()
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,8 +1,8 @@
 const timer = (deadline) => {
-    const timerDays = document.querySelector('.count_1 > span');
-    const timerHours = document.querySelector('.count_2 > span');
-    const timerMinutes = document.querySelector('.count_3 > span');
-    const timerSeconds = document.querySelector('.count_4 > span');
+    const timerDays = document.querySelectorAll('.count_1 > span');
+    const timerHours = document.querySelectorAll('.count_2 > span');
+    const timerMinutes = document.querySelectorAll('.count_3 > span');
+    const timerSeconds = document.querySelectorAll('.count_4 > span');
 
     const getTimeRemaining = () => {
         let dateStop = new Date(deadline).getTime();
@@ -23,21 +23,31 @@ const timer = (deadline) => {
         return num;
     };
 
+    const changeTime = (elems, newtext) => {
+        elems.forEach(elem => {
+            elem.textContent = newtext;
+        })
+    }
+
     const updateClock = () => {
         let getTime = getTimeRemaining();
         getTime.hours = addZero(getTime.hours);
         getTime.minutes = addZero(getTime.minutes);
         getTime.seconds = addZero(getTime.seconds);
         if (getTime.timeRemaining > 0) {
-            timerDays.textContent = getTime.days;
+            changeTime(timerDays, getTime.days)
+            changeTime(timerHours, getTime.hours)
+            changeTime(timerMinutes, getTime.minutes)
+            changeTime(timerSeconds, getTime.seconds)
+
+            /* timerDays.textContent = getTime.days;
             timerHours.textContent = getTime.hours;
             timerMinutes.textContent = getTime.minutes;
-            timerSeconds.textContent = getTime.seconds;
+            timerSeconds.textContent = getTime.seconds; */
         }
     };
 
     setInterval(updateClock, 1000);
-
 }
 
 export default timer
